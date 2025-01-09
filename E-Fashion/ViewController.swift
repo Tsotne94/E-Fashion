d6649fa2780f8f70a98112bc3a82d340e24f2133
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2025-01-09 19:02:04 +0400

    first commit

---
+++ E-Fashion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-Fashion
+//
+//  Created by Cotne Chubinidze on 09.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
