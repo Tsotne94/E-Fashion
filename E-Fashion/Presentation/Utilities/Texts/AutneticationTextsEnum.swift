bf391f5850e99f537f1bcf86622e1122468a6746
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2025-01-17 22:04:22 +0400

    feat: Add AuthenticationTextsEnum for handling authentication-related texts

---
+++ E-Fashion/Presentation/Utilities/Texts/AutneticationTextsEnum.swift
@@ -0,0 +1,17 @@
+//
+//  AutneticationTextsEnum.swift
+//  E-Fashion
+//
+//  Created by Cotne Chubinidze on 17.01.25.
+//
+enum SignInPageTexts {
+    
+}
+
+enum SignUpPageTexts {
+    
+}
+
+enum ForgotPasswordPageTexts {
+    
+}
